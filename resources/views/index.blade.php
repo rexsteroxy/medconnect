7f61bae82c3bb7488cfe086c07e0c402a7b0f00a
rexsteroxy <rexsteroxy22@gmail.com>
2019-10-15 22:39:39 +0100

    i added some pictures

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,11 +75,13 @@
 	<!-- Departments -->
 
 	<div class="departments">
-		<div class="departments_background parallax-window" data-parallax="scroll" data-image-src="images/departments.jpg" data-speed="0.8"></div>
+		<div class="departments_background parallax-window" data-parallax="scroll" 
+		data-image-src="images/departments.jpg" data-speed="0.8"></div>
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<div class="section_title section_title_light"><h2>We Provide All Kinds Of Medical Job Opportunities</h2></div>
+					<div class="section_title section_title_light">
+					<h2>We Provide All Kinds Of Medical Job Opportunities</h2></div>
 				</div>
 			</div>
 			<div class="row departments_row row-md-eq-height">
